57bf8d933f066f8710a4d5f4899e0172891e8799
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-06-18 11:14:48 +0200

    add quiz game logic

--- 21-python-projects/quiz-game.py
+++ 21-python-projects/quiz-game.py
@@ -1,1 +1,40 @@
 print('Welcome to my quiz')
+
+playing = input('Do you want to play? ')
+
+if playing.lower() != 'yes':
+    quit()
+
+print('Okay! Lets play')
+score = 0
+
+answer = input('What does CPU stand for? ' ).lower()
+if answer == 'center processing unit':
+    print('Correct!')
+    score += 1
+else: 
+    print('Incorrect!')
+
+answer = input('What does GPU stand for? ' ).lower()
+if answer == 'graphics processing unit':
+    print('Correct!')
+    score += 1
+else: 
+    print('Incorrect!')
+
+answer = input('What does RAM stand for? ' ).lower()
+if answer == 'random acces memory':
+    print('Correct!')
+    score += 1
+else: 
+    print('Incorrect!')
+
+answer = input('What does PSU stand for? ' ).lower()
+if answer == 'powwer supply':
+    print('Correct!')
+    score += 1
+else: 
+    print('Incorrect!')
+
+print('You got ' + str(score) + ' question correct')
+print('You got ' + str((score / 4) * 100) + '%')
