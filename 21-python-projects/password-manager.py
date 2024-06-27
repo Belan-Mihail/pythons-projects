eb1109666e0d4d9a983067a7ddc2c9568148608c
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-06-27 21:31:02 +0200

    def add function to add user name and password

--- 21-python-projects/password-manager.py
+++ 21-python-projects/password-manager.py
@@ -1,4 +1,22 @@
-pwd = input('What is the master password? ')
+master_pwd = input('What is the master password? ')
+
+def view():
+    pass
+
+def add():
+    name = input('Account Name: ')
+    pwd = input('Account Password: ')
+ 
+    # automatically close this file
+    # a mode allow us to add data to the end if file exist or create new file
+    # r mode allow us only read file
+    # w mode overwrite (clear and create new one) all data if file already exist
+    # f it is name off our file 
+    with open('password.txt', 'a') as f:
+        f.write(name + '|' + pwd)
+
+
+
 
 while True:
     mode = input(
@@ -7,9 +25,9 @@ while True:
         break
 
     if mode == 'view':
-        pass
+        view()
     elif mode == 'add':
-        pass
+        add()
     else: 
         print('Invalid mode')
         continue
