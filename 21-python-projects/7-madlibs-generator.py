ad333de9c53dafaef253952b0904325f85c72e7c
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-07 21:53:17 +0200

    add logic to add all changed world from story to the words list

---
+++ 21-python-projects/7-madlibs-generator.py
@@ -0,0 +1,23 @@
+with open('story.txt', 'r') as f:
+   story = f.read() 
+
+words = []
+start_of_world = -1
+target_start = '<'
+target_end = '>'
+
+
+'''
+So, the enumerate() function in Python takes a set of data as a parameter and returns an enumerate object.
+This object is returned in the format of key-value pairs, where the keys are the corresponding indexes of the elements, 
+and the values ​​are the elements themselves from the passed data set.
+'''
+for i, char in enumerate(story):
+   if char == target_start:
+      start_of_world = i
+   
+   if char == target_end and start_of_world != -1:
+    #   slice (start index = start_of_world, end index = i + 1)
+      word = story[start_of_world: i + 1]
+      words.append(word)
+      start_of_world = -1
