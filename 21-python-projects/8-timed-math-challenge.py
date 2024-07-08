8c284da82ed43f19cf7ecefab31c9d945ff66f55
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-08 22:57:41 +0200

    finish with project 8

--- 21-python-projects/8-timed-math-challenge.py
+++ 21-python-projects/8-timed-math-challenge.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 OPERATORS = ['+', '-', '*']
 MIN_OPERAND = 3
@@ -17,9 +18,21 @@ def generate_problem():
 # expr, answer = generate_problem()
 # print(expr, answer)
 
+wrong = 0
+input('Press enter to start')
+
+start_time = time.time()
+
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
     while True:
-        guess = input('Problem #' + str(i + 1) + ': ' + expr + ' = ')
+        guess = input('Problem number' + str(i + 1) + ':  ' + expr + ' = ')
         if guess == str(answer):
             break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print('nice work!!')
+print('You finished in ', total_time, 'second!')
