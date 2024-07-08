ab3bfca688f6e96f3b0387032d4b86f609441c54
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-08 22:43:38 +0200

    generate 10 random question for a guess

--- 21-python-projects/8-timed-math-challenge.py
+++ 21-python-projects/8-timed-math-challenge.py
@@ -3,6 +3,7 @@ import random
 OPERATORS = ['+', '-', '*']
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -10,7 +11,15 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + '' + operator + '' + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+# expr, answer = generate_problem()
+# print(expr, answer)
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input('Problem #' + str(i + 1) + ': ' + expr + ' = ')
+        if guess == str(answer):
+            break
