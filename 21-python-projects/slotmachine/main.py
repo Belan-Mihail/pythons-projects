1c62f6de5bc62f85979c01f9813ad75e05cafc39
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-10 10:25:40 +0200

    fix small bugs

--- 21-python-projects/slotmachine/main.py
+++ 21-python-projects/slotmachine/main.py
@@ -23,15 +23,32 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(symbol_count):
             all_symbols.append(symbol)
     
-    columns = [[], [], []]
-    for col in range(cols):
+    columns = []
+    # generate a column
+    for _ in range(cols):
         column = []
         # [:] - slice meth to copy list all_symbols in list current_symbols
         current_symbols = all_symbols[:]
-        for row in range(rows):
+        # generate rows
+        for _ in range(rows):
             value = random.choice(current_symbols)
             # we remove value from current_symbol. so that value is not repeated more than allow number times 
             current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    # loop durch every single row
+    for row in range(len(columns[0])):
+        # loop every colum
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=' | ')
+            else:
+                print(column[row], end='')
+        print()
+
 
 
 
@@ -66,7 +83,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        bet = input(f'Enter your bet. Amount must be between ${MIN_BET} - ${MAX_BET}  ')
+        bet = input("What would you like to bet on each line? $")
         if bet.isdigit():
 
             bet = int(bet)
@@ -76,6 +93,7 @@ def get_bet():
                 print(f'Amount must be between ${MIN_BET} - ${MAX_BET}  ')
         else:
             print('Invalid value')
+    return bet
 
 
 def main():
@@ -92,5 +110,7 @@ def main():
 
     print(f'Your are betting ${bet} on ${lines} lines- Total bet is equal to: ${total_bet}')
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
