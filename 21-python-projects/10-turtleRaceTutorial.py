c83dc459e1088d7b7c55c1333bef053c0287acdb
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-15 10:50:34 +0200

    move racer steps

--- 21-python-projects/10-turtleRaceTutorial.py
+++ 21-python-projects/10-turtleRaceTutorial.py
@@ -1,10 +1,9 @@
 import turtle
+import time
 
 WIDTH, HEIGHT = 500, 500
 
-# create turtle screen
-screen = turtle.Screen()
-screen.setup(WIDTH, HEIGHT)
+
 
 
 def get_number_of_racers():
@@ -22,4 +21,26 @@ def get_number_of_racers():
 		else:
 			print('Number not in range 2-10. Try Again!')
 
+
+def init_turtle():
+	# create turtle screen
+    screen = turtle.Screen()
+	# middle position will be x:0 y:0. top-middle: x:0 y:250px (HEIGHT / 2). bottom-middle: x:0 y:-250px (HEIGHT / 2)
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!')
+
 racers = get_number_of_racers()
+init_turtle()
+
+# we use this module to can change the object
+racer = turtle.Turtle()
+
+# racer will move forward for 100 px
+racer.forward(100)
+# 90 degree to turn to the left
+racer.left(90)
+# racer will move forward for 100 px
+racer.forward(100)
+# 90 degree to turn ti the right
+racer.right(90)
+time.sleep(5)
