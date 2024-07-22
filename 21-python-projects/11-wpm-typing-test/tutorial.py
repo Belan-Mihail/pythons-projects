86d71881846aa33752b95a3a00ee36e0704f97da
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-22 06:42:06 +0200

    add color function

--- 21-python-projects/11-wpm-typing-test/tutorial.py
+++ 21-python-projects/11-wpm-typing-test/tutorial.py
@@ -2,8 +2,11 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    # 1 = id, add front and background color
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
     stdscr.clear()
-    stdscr.addstr('Hello world')
+    # use curses.init_pair with id 1 to follow text
+    stdscr.addstr('Hello world', curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
 
