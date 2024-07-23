a80345a81cdaafeac63865e5ee886afd30e11f01
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-23 06:35:25 +0200

    def wpm_text funt

--- 21-python-projects/11-wpm-typing-test/tutorial.py
+++ 21-python-projects/11-wpm-typing-test/tutorial.py
@@ -24,12 +24,21 @@ def start_screen(stdscr):
 #     key = stdscr.getkey()
 #     print(key)
 
+def wpm_test(stdscr):
+    target_text = 'Hello world this is some text for this app'
+    current_text = []
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 
 wrapper(main)
